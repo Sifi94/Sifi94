87be7e0d0763477b3b348583f62fd93389aa1dc8
Sifi94 <oliver.schiffer@barbershopmom.com>
2022-10-11 10:40:38 +0200

    nem váltó műtét

---
+++ gyakorlas.js
@@ -0,0 +1,9 @@
+const person = {
+    Name:"Gizi"
+}
+function changename(p) {
+    p.Name="Imre"
+}
+console.log(person.Name)
+changename(person);
+console.log(person.Name)
---
+++ parameter.js
@@ -0,0 +1,11 @@
+//paraméter átadás
+
+let a = 10;
+function plusfive(x) {
+    x = x + 5;
+    console.log(x)
+}
+plusfive(a);
+console.log(a);
+
+// 
