268da6ede03eb5039036e6016e999f6a5d40d032
Sifi94 <oliver.schiffer@barbershopmom.com>
2022-10-11 10:21:59 +0200

    további gyakorlás

---
+++ arrowfunction.js
@@ -0,0 +1,11 @@
+const summary = (n)=> {
+    let c=0;
+for(let i=1; i<11; i++); {
+c = c+i
+//i++ gyűjtőgeti az értékeket like...1,2,3,4,5,6,7,8,9,10
+// c = c+i ezt az értéket hozzá adja az előzőhöz 
+} return c;
+}
+
+//summary();
+console.log(summary());
---
+++ function2.js
@@ -0,0 +1,8 @@
+const display = function (n) {
+    for (let i=1; i<n; i++) {
+        console.log(i);
+    }
+}
+display(20);
+
+// aro function
