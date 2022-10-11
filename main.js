96846b5e8ff3ed5594c4e12039db21e044b10692
Sifi94 <oliver.schiffer@barbershopmom.com>
2022-10-11 09:54:04 +0200

    testing

---
+++ main.js
@@ -0,0 +1,12 @@
+
+
+function calculate(a, b) {
+    var c;
+    c = a*b;
+    //console.log(c)
+    return c;
+}
+console.log(calculate(6,5));
+// paraméter függő lesz, ha a és b-t rakjuk a függvény gyomrába
+
+
